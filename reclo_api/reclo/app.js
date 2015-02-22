bd8c8079d514fd82e3e2e3ea0a9fd339bcc99344
Carlton Duffett <cjduffett@comcast.net>
2015-02-22 17:47:19 -0500

    Added REST routes for login, logout, register

--- reclo_api/reclo/app.js
+++ reclo_api/reclo/app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var login = require('./routes/login');
+var logout = require('./routes/logout');
+var register = require('./routes/register');
 
 var app = express();
 
@@ -23,7 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/login',login);
+app.use('/logout',logout);
+app.use('/register',register);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
