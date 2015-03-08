e3c4114c4dc1a0ff536504828d307f410df9cfcf
Carlton Duffett <cjduffett@comcast.net>
2015-03-08 13:18:17 -0400

    Changed logout to use MySQL database

--- api/routes/logout.js
+++ api/routes/logout.js
@@ -1,85 +1,99 @@
-// ReClo: user_authentication
-// --------------------------
+// ReClo: /ua/logout
+// -----------------
 // API Logout Script
-// v1.0.0
+// v2.0.0
 // Carlton Duffett
-// 2-25-2015
+// 3-8-2015
 
 var express = require('express');
-var router = express.Router();
+var mysql = require('mysql');
+var http = require('http');
+var bl = require('bl');
 var corelib = require('../lib/core.js');
+var router = express.Router();
 
-// connect to AWS DynamoDB
-var AWS = require('aws-sdk');
-AWS.config.region = 'us-west-2';
-var db = new AWS.DynamoDB();
+// Response Error Codes:
+// --------------------
+// 1 = System Error (MySQL query error, database connection error, etc.)
+// 2 = User Error (invalid user credentials, user not found, etc.)
 
-/* Logout user and invalidate token */
-router.post('/', function(req, res) {
+/***************************************************************************************/
+/* FUNCTION DEFINITIONS                                                                */
+/***************************************************************************************/
 
-	// process request
-	var token = req.body.token;
+// connect to MySQL Database using user-data password
+function openDBConnection(res,token) {
+    
+    // Amazon RDS host address
+    var host = corelib.getMySQLHost().toString();
+    var url = "http://169.254.169.254/latest/user-data";
 
-	// invalidate token in Token table
+    // get password securely
+    http.get(url, function handleResponse(pwres){
 
-	// query database to check if token already exists
-	var params = {
-        TableName : 'tokens',
-        KeyConditions : { 
-            "token_id" : {
-            "AttributeValueList" : [ 
-                {"S" :  token}
-                ],
-            "ComparisonOperator" : "EQ",
+        pwres.pipe(bl(function(err,data){
+
+            if (err) {
+                console.error('There was an error getting db password: ' + err);
+                res.json({success: 0, error: 1, msg:'Failed to obtain database password'}); // Error 1: MySQL Error
+                res.send();
             }
-        }
-    }
+            else {
+                var pw = data.toString().slice(5);
 
-	// query DynamoDB for desired token
-    db.query(params, function(err, data) {
-        if (err) {
-            console.log('Query Error: ' + err); // an error occurred
-            res.json({success:0, error:1, msg:"query Error: token not found"});
-            res.send();
-      	}
-      	else {
-      		if (data.Count > 0) {
-      			// token exists and okay to deactivate
-      			deactivateToken(token,res);
-      		}
-      	}
+                // connect to ReClo databse
+                var db = mysql.createConnection({
+                    host     : host,
+                    port     : '3306',
+                    user     : 'reclo',
+                    password : pw,
+                    database : 'reclodb',
+                });
+                db.connect();
+
+                // proceed with user verification
+                deactivateToken(res,db,token);
+            }
+        }));
     });
-});
-
+}
 
 // change token_status to disabled 'D'
-function deactivateToken(token,res) {
+function deactivateToken(res,db,token) {
 
-	// updateItem parameters
-	var params = {
-			TableName: "tokens",
-			Key:{
-				token_id:{"S": token}
-			},
-			AttributeUpdates: {
-				token_status:{
-					Value:{"S": 'D'}
-				}
-			}
-		};
+    var timestamp = corelib.createTimestamp();
+    var qry = "UPDATE reclodb.tokens SET token_status = 'D', date_deactivated = ? WHERE token_id = ?";
 
-	db.updateItem(params, function(err, data) {
-  		if (err) {
-  			console.log('updateItem Error: ' + err); // an error occurred
-  			res.json({success: 0, error: 1, msg:"updateItem Error: token deactivation failed"}); // Error 1: table update failed
-  			res.send();
-  		}
-  		else {
-  			console.log('updateItem successful: token disabled'); // successful response
-  			res.json({success: 1, error: 0, msg:'logout successful: token disabled'});
-  			res.send();
-  		}
-	});
+    db.query(qry,[timestamp,token],function(err,results){
+
+        if (err) {
+            console.log('deactivateToken ' + err);
+            res.json({success: 0, error: 1, msg:'MySQL deactivateToken query failed'}); // Error 1: MySQL error
+            res.send();
+            closeDBConnection(db);
+        }
+        else {
+            console.log('logoutUser successful!');
+            res.json({success: 1, error: 0, msg:'logout successful'});
+            res.send();
+
+            // disconnect from database
+            closeDBConnection(db);
+        }
+    });
 }
+
+/***************************************************************************************/
+/* REQUEST HANDLING                                                                    */
+/***************************************************************************************/
+
+router.post('/', function(req, res) {
+
+    // process request
+    var token = req.body.token;
+
+    // invalidate token in Token table
+    openDBConnection(res,token);
+});
 
 module.exports = router;
