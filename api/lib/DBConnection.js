448fff87221fee8a2564427766d6f78be87d4642
Carlton Duffett <cjduffett@comcast.net>
2015-03-17 23:47:05 -0400

    Asynchronous bug fix

--- api/lib/DBConnection.js
+++ api/lib/DBConnection.js
@@ -5,47 +5,54 @@ var http = require('http');
 var bl = require('bl');
 
 // Constructor
-function DBConnection(errCallback) {
+function DBConnection() {
 
-	var that = this;
-	that.host = 'reclodb.ctng2ag7pnrb.us-west-2.rds.amazonaws.com';
-	that.url = 'http://169.254.169.254/latest/user-data';
-
-	// get password securely from EC2 userdata
-	function handleResponse(response) {
-
-		// connect to MySQL database
-		function connectToDB(err,data) {
-
-			if (err) {
-                errCallback(err);
-              	return;
-            }
-
-	        var pw = data.toString().slice(5);
-
-	        // connect to ReClo databse
-	        that.db = mysql.createConnection({
-	            host     : that.host,
-	            port     : '3306',
-	            user     : 'reclo',
-	            password : pw,
-	            database : 'reclodb',
-	        });
-	        that.db.connect();
-
-		}; // connectToDB
-		response.pipe(bl(connectToDB));
-
-    }; // handleResponse
-    http.get(that.url,handleResponse);
+    this.host = 'reclodb.ctng2ag7pnrb.us-west-2.rds.amazonaws.com';
+    this.url = 'http://169.254.169.254/latest/user-data';
 }
 
-// Member functions
+// Initiate connection to MySQL database
+DBConnection.prototype.connect = function(connectionCallback) {
+
+    var that = this;
+
+    // get password securely from EC2 userdata
+    function handleResponse(response) {
+
+        // connect to MySQL database
+        function connectToDB(err,data) {
+
+            if (err) {
+                connectionCallback(err);
+                return;
+            }
+
+            var pw = data.toString().slice(5);
+
+            // connect to ReClo databse
+            that.db = mysql.createConnection({
+                host     : that.host,
+                port     : '3306',
+                user     : 'reclo',
+                password : pw,
+                database : 'reclodb',
+            });
+            that.db.connect();
+
+            connectionCallback(0);
+
+        }; // connectToDB
+        response.pipe(bl(connectToDB));
+
+    }; // handleResponse
+    http.get(this.url,handleResponse);
+}
+
+// Perform MySQL query
 DBConnection.prototype.query = function(qry,params,callback) {
 
-	// MySQL query
-	this.db.query(qry,params,callback);
+    // MySQL query
+    this.db.query(qry,params,callback);
 }
 
 // Export the class
--- api/routes/register.js
+++ api/routes/register.js
@@ -18,17 +18,17 @@ var router = express.Router();
  * Res Params:
  * -----------
  * On error:    error
- * On success:  message 
+ * On success:  message
  *
  * Validates provided username, email, and password, then
  * Creates a new entry in the user database.
  */
 router.post('/', function(req,res) {
-  
+
     var username = req.body.username;
     var password = req.body.password;
     var email    = req.body.email;
-    
+
     // validate form information
     var tests = [];
     tests[0] = corelib.validateUsername(username);
@@ -61,66 +61,72 @@ router.post('/', function(req,res) {
         return;
     }
 
-    // what DBConnection should do in the event of a connection error
-    function errCallback(err) {
-        console.log('There was an error getting db password: ' + err);
-        res.status(500).json({error: 'There was an error connecting to the database'});
-    }; // errCallback
-
-    // Open MySQL database connection
-    var db = new DBConnection(errCallback);
-    
-    // check that user does not already exist
-    var qry = "SELECT email FROM reclodb.users WHERE email = ? AND user_status = 'A'"
-    var params = [email];
-
-    function checkUserExistenceCallback(err,results) {
+    // what DBConnection should do after connection is established
+    function connectionCallback(err) {
 
         if (err) {
-            console.log('checkUserExistence ' + err);
-            res.status(500).json({error:'There was an error connecting to the database'}); // MySQL error
+            console.log('There was an error getting db password: ' + err);
+            res.status(500).json({error: 'There was an error connecting to the database'});
             return;
         }
 
-        if (results[0] != null) {
-            // user already exists with that email address
-            console.log('Error: User already exists');
-            res.status(500).json({error: 'User with that email aleady exists'}); // User already exists
-            return;
-        }
+        // check that user does not already exist
+        var qry = "SELECT email FROM reclodb.users WHERE email = ? AND user_status = 'A'"
+        var params = [email];
 
-        // insert new user into the database
-        var qry = 'INSERT INTO reclodb.users SET ?';
-
-        var uuid = corelib.generateUUID();
-        var hashed_password = corelib.hashPassword(password);
-        var timestamp = corelib.createTimestamp();
-
-        var params = {
-            'user_id'       : uuid, 
-            'username'      : username,
-            'email'         : email,
-            'hash'          : hashed_password,
-            'date_created'  : timestamp,
-            'user_status'   : 'A',
-        };
-
-        function registerUserCallback(err,results) {
+        function checkUserExistenceCallback(err,results) {
 
             if (err) {
-                console.log('registerUser ' + err);
+                console.log('checkUserExistence ' + err);
                 res.status(500).json({error:'There was an error connecting to the database'}); // MySQL error
                 return;
             }
 
-            console.log('registerUser successful!');
-            res.status(200).json({message:'New user created'});
+            if (results[0] != null) {
+                // user already exists with that email address
+                console.log('Error: User already exists');
+                res.status(500).json({error: 'User with that email aleady exists'}); // User already exists
+                return;
+            }
 
-        }; // registerUserCallback
-        db.query(qry,params,registerUserCallback);
+            // insert new user into the database
+            var qry = 'INSERT INTO reclodb.users SET ?';
 
-    }; // checkUserExistenceCallback
-    db.query(qry,params,checkUserExistenceCallback);
+            var uuid = corelib.generateUUID();
+            var hashed_password = corelib.hashPassword(password);
+            var timestamp = corelib.createTimestamp();
+
+            var params = {
+                'user_id'       : uuid,
+                'username'      : username,
+                'email'         : email,
+                'hash'          : hashed_password,
+                'date_created'  : timestamp,
+                'user_status'   : 'A',
+            };
+
+            function registerUserCallback(err,results) {
+
+                if (err) {
+                    console.log('registerUser ' + err);
+                    res.status(500).json({error:'There was an error connecting to the database'}); // MySQL error
+                    return;
+                }
+
+                console.log('registerUser successful!');
+                res.status(200).json({message:'New user created'});
+
+            }; // registerUserCallback
+            db.query(qry,params,registerUserCallback);
+
+        }; // checkUserExistenceCallback
+        db.query(qry,params,checkUserExistenceCallback);
+
+    }; // connectionCallback
+
+    // Open MySQL database connection
+    var db = new DBConnection();
+    db.connect(connectionCallback.bind(db));
 
 }); // router
 
