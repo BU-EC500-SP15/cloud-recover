4f2171cbf12739416c01349ddbabb9c3747cdecc
Konstantino Sparakis <ksparakis@gmail.com>
2015-03-19 05:29:49 -0400

    C# client http method class

--- clients/client_lib/RecloApiCaller.cs
+++ clients/client_lib/RecloApiCaller.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Net.Http;
 
+// command for compiling with MONO 
+//  mcs /reference:System.ServiceModel.Web.dll /reference:System.Net.Http /reference:System.Runtime.Serialization *
 public class RecloApiCaller
 {
     
-    private static string urlHead = "reclo.noip.me";
-    private static string loginUrl = urlHead+"/login";
-    private static string registerUrl = urlHead+"/register";
-    private static string logoutUrl = urlHead+"/logout";
+    private static string urlHead = "http://52.11.1.237:3000/";
+    //private static string urlHead = "http://stutorapi.sparakis.com";
+    private static string loginUrl = "login";
+    private static string registerUrl = "register";
+    private static string logoutUrl = "logout";
 
     /*
      * NOTE : all callback functons need to take as a parameter :
@@ -26,19 +31,10 @@ public class RecloApiCaller
 
     }
 
-    
-    public void loginUser(string email, string password,
-                     Action<HttpWebRequestCallbackState> callBack , object state = null)
-    {
-        
-        NameValueCollection postParameters = new NameValueCollection();
-        postParameters.Add("email", email);
-        postParameters.Add("password", password);
-    
-    	 HttpSocket.PostAsync(loginUrl, postParameters, callBack, state);
-    }
-
-    public void registerUser(string email, string username, string password,
+/*
+ * EXAMPLE USING HTTPSCOKET.cs which doesnt work
+ 
+    public static void registerUser(string email, string username, string password,
                   Action<HttpWebRequestCallbackState> callBack , object state = null)
     {
         
@@ -49,14 +45,74 @@ public class RecloApiCaller
     
         HttpSocket.PostAsync(registerUrl, postParameters, callBack, state);
     }
-
-    public void logoutUser(string token,  Action<HttpWebRequestCallbackState> callBack , object state = null)
-    {
-        
-    	NameValueCollection postParameters = new NameValueCollection();
-        postParameters.Add("token", token);
+    */
     
-         HttpSocket.PostAsync(logoutUrl, postParameters, callBack, state);
+
+
+
+    public static void loginUser(string email, string password,
+                     Action<string> callBack , object state = null)
+    {
+         Console.Write("login Initiated\n");
+    
+          using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri(urlHead);
+            var content = new FormUrlEncodedContent(new[] 
+            {
+                new KeyValuePair<string, string>("email", email),
+                new KeyValuePair<string, string>("password", password)
+            });
+
+            var result = client.PostAsync("login", content).Result;
+            string resultContent = result.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(resultContent);
+            callBack(resultContent);
+        }
+        
+    	
+    }
+
+
+
+    public static void registerUser(string email, string password, string username,  Action<string> callBack , object state = null)
+    {
+           using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri(urlHead);
+            var content = new FormUrlEncodedContent(new[] 
+            {
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("email", email),
+                new KeyValuePair<string, string>("password", password)
+
+            });
+
+            var result = client.PostAsync("login", content).Result;
+            string resultContent = result.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(resultContent);
+            callBack(resultContent);
+        }
+        
+    }
+
+
+    public static void logoutUser(string token,  Action<string> callBack , object state = null)
+    {
+           using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri(urlHead);
+            var content = new FormUrlEncodedContent(new[] 
+            {
+                new KeyValuePair<string, string>("token", token)
+            });
+
+            var result = client.PostAsync("login", content).Result;
+            string resultContent = result.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(resultContent);
+            callBack(resultContent);
+        }
+        
     }
 
 
