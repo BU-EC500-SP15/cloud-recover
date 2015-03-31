d59bcee8b22df1e4f85b182f17dfca76ca42a449
Carlton Duffett <cjduffett@comcast.net>
2015-03-31 17:36:09 -0400

    Updates
    Updated completeUpload() function
    Added deleteBackup() function (still requires testing)

--- api/routes/backups.js
+++ api/routes/backups.js
@@ -319,8 +319,6 @@ router.put('/uploads/:user_id/:upload_id', function(req, res) {
     var user_id         = req.params.user_id;
     var upload_id       = req.params.upload_id;
     var upload_status   = req.body.upload_status;
-    var file_name       = req.body.file_name;
-    var file_size       = req.body.file_size;
     var token           = req.query.token;
 
     function validationCallback(err) {
@@ -363,45 +361,60 @@ router.put('/uploads/:user_id/:upload_id', function(req, res) {
 
                 console.log('completeUpload successful.');
 
-                // add new backup to backups table if upload_status = 'S'
-                if (upload_status == 'S') {
+                // get file_name and file_size from the uploads table
+                var qry = "SELECT file_name, file_size FROM reclodb.uploads WHERE upload_id = ?";
+                var params = [upload_id];
 
-                    // backup upload was successful, create new backup entry
-                    var qry = "INSERT INTO reclodb.backups SET ?";
+                function getBackupInfoCallback(err,results) {
 
-                    var backup_id = corelib.createToken();
-                    var timestamp = corelib.createTimestamp();
+                    if (err) {
+                        console.log('getBackupInfo ' + err);
+                        res.status(500).json({error: 'Failed to get backup information'}); // MySQL error
+                        return;
+                    }
 
-                    var params = {
-                        'backup_id'     : backup_id,
-                        'user_id'       : user_id,
-                        'file_name'     : file_name,
-                        'file_size'     : file_size,
-                        'date_created'  : timestamp,
-                        'backup_status' : 'A',
-                    };
+                    var file_name = results[0].file_name;
+                    var file_size = results[0].file_size;
 
-                    function createBackupCallback(err,results) {
+                    // add new backup to backups table if upload_status = 'S'
+                    if (upload_status == 'S') {
 
-                        if (err) {
-                            console.log('createBackup ' + err);
-                            res.status(500).json({error:'There was an error connecting to the database'}); // MySQL error
-                            return;
-                        }
+                        var backup_id = corelib.createToken();
+                        var timestamp = corelib.createTimestamp();
 
-                        console.log('Backup upload successful. File: ' + file_name);
-                        res.status(200).json({message: 'Backup created'});
+                        // backup upload was successful, create new backup entry
+                        var qry = "INSERT INTO reclodb.backups SET ?";
 
-                    } // createBackupCallback
-                    db.query(qry,params,createBackupCallback);
+                        var params = {
+                            'backup_id'     : backup_id,
+                            'user_id'       : user_id,
+                            'file_name'     : file_name,
+                            'file_size'     : file_size,
+                            'date_created'  : timestamp,
+                            'backup_status' : 'A',
+                        };
 
-                }
-                else {
+                        function createBackupCallback(err,results) {
 
-                    // backup upload was unsuccessful
-                    console.log('Backup upload failed. File: ' + file_name);
-                    res.status(204).send(); // 204, no content
-                }
+                            if (err) {
+                                console.log('createBackup ' + err);
+                                res.status(500).json({error:'There was an error connecting to the database'}); // MySQL error
+                                return;
+                            }
+
+                            console.log('Backup upload successful. File: ' + file_name);
+                            res.status(200).json({message: 'Backup created'});
+
+                        } // createBackupCallback
+                        db.query(qry,params,createBackupCallback);
+                    }
+                    else {
+                        // backup upload was unsuccessful
+                        console.log('Backup upload confirmed failed. File: ' + file_name);
+                        res.status(200).send({message: 'Backup upload confirmed failed'}); // no content
+                    }
+                } // getBackupInfoCallback
+                db.query(qry,params,getBackupInfoCallback);
 
             }; // completeUploadCallback
             db.query(qry,params,completeUploadCallback);
@@ -433,9 +446,80 @@ router.put('/uploads/:user_id/:upload_id', function(req, res) {
  */
 router.delete('/:user_id/:backup_id', function(req,res) {
 
-    var user_id = req.params.user_id;
-    var backup_id = req.params.backup_id;
+    var user_id     = req.params.user_id;
+    var backup_id   = req.params.backup_id;
+    var user_id     = req.params.user_id;
+    var backup_id   = req.params.backup_id;
+    var token       = req.query.token;
 
+    function validationCallback(err) {
+
+        if (err) {
+            console.log('Invalid token.');
+            res.status(500).json({error: 'Invalid token.'});
+            return;
+        }
+
+        // what DBConnection should do after connection is established
+        function connectionCallback(err) {
+
+            if (err) {
+                console.log('There was an error getting db password: ' + err);
+                res.status(500).json({error: 'There was an error connecting to the database'});
+                return;
+            }
+
+            // get filename (key) for backup to delete
+            var qry = "SELECT file_name FROM reclodb.backups WHERE backup_id = ?";
+            var params = [backup_id];
+
+            function getFileKeyCallback(err,results) {
+
+                if (err) {
+                    console.log('getFileKey ' + err);
+                    res.status(500).json({error: 'Failed to obtain file_name. Cannot complete delete.'}); // MySQL error
+                    return;
+                }
+
+                if (results.length == 0) {
+                    console.log('No backup with id = ' + backup_id + ' found');
+                    res.status(500).json({error: 'No backup found'}); // MySQL error
+                    return;
+                }
+
+                // use file_name to delete object in S3
+                var s3 = new AWS.S3();
+                var bucket = 'reclo-client-backups/';
+                var file_name = user_id + '/' + results[0].file_name;
+
+                var params = {
+                    Bucket  : bucket,
+                    Key     : file_name
+                };
+
+                function deleteBackupCallback(err,data) {
+
+                    if (err) {
+                        console.log('deleteBackup Error: ' + err);
+                        res.status(500).json({error: 'Failed to delete backup'});
+                        return;
+                    }
+
+                    console.log('deleteBackup successful.');
+                    res.status(200).json({message: 'Delete backup successful'});
+                }
+                s3.deleteObject(params,deleteBackupCallback);
+            }
+            db.query(qry,params,getFileKeyCallback);
+
+        }; // connectionCallback
+
+        // Open MySQL database connection
+        var db = new DBConnection();
+        db.connect(connectionCallback.bind(db));
+
+    } // validationCallback
+    corelib.validateToken(token,validationCallback);
 });
 
 module.exports = router;
