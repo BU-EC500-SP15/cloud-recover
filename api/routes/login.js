e55296ea096b2ad5126d12c9878c92947529d143
Carlton Duffett <cjduffett@comcast.net>
2015-04-16 02:20:51 -0400

    added username to JSON response
    Login response now includes username;
    Reformatted file for readability

--- api/routes/login.js
+++ api/routes/login.js
@@ -25,171 +25,173 @@ var router = express.Router();
  */
 router.post('/', function(req, res) {
 
-    // process request
-    var email = req.body.email;
-    var password = req.body.password;
+// process request
+var email = req.body.email;
+var password = req.body.password;
 
-    // validate form information
-    var tests = [];
-    tests[0] = corelib.validatePassword(password);
-    tests[1] = corelib.validateEmail(email);
+// validate form information
+var tests = [];
+tests[0] = corelib.validatePassword(password);
+tests[1] = corelib.validateEmail(email);
 
-    var allValid = true;
+var allValid = true;
 
-    for (i = 0; i < tests.length; i++){
-        if (tests[i] == false){
-            allValid = false;
-            break;
-        }
+for (i = 0; i < tests.length; i++){
+    if (tests[i] == false){
+        allValid = false;
+        break;
+    }
+}
+
+if (!allValid) {
+
+    var emsg = 'Invalid ';
+    var error = 0;
+    if (tests[0] == false) {
+        console.log('Validation Error: invalid password format.');
+        error = 209;
+        emsg = emsg + 'password format, ';
+    }
+    if (tests[1] == false) {
+        console.log('Validation Error: invalid email format.');
+        error = 208;
+        emsg = emsg + 'email format';
     }
 
-    if (!allValid) {
+    console.log('Validation Error: ' + emsg);
+    res.status(500).json({error: error, message: emsg});
+    return;
+}
 
-        var emsg = 'Invalid ';
-        var error = 0;
-        if (tests[0] == false) {
-            console.log('Validation Error: invalid password format.');
-            error = 209;
-            emsg = emsg + 'password format, ';
-        }
-        if (tests[1] == false) {
-            console.log('Validation Error: invalid email format.');
-            error = 208;
-            emsg = emsg + 'email format';
-        }
+// what DBConnection should do in the event of a connection error
+function connectionCallback(err) {
 
-        console.log('Validation Error: ' + emsg);
-        res.status(500).json({error: error, message: emsg});
+    if (err) {
+        console.log('There was an error connecting to the database: ' + err);
+        res.status(500).json({error: 101, message: 'There was an error connecting to the database'});
+        db.disconnect();
         return;
     }
 
-    // what DBConnection should do in the event of a connection error
-    function connectionCallback(err) {
+    // verify user in database
+    var qry = "SELECT * FROM reclodb.users WHERE email = ? AND user_status = 'A'";
+    var params = [email];
+
+function verifyUserCallback(err,results) {
+
+    if (err) {
+        console.log('verifyUser ' + err);
+        res.status(500).json({error: 101, message: 'There was an error connecting to the database'}); // MySQL error
+        db.disconnect();
+        return;
+    }
+
+    if (results.length == 0) {
+        // user not found
+        console.log('Error: User not found');
+        res.status(500).json({error: 206, message:'User not found'});
+        db.disconnect();
+        return;
+    }
+
+    var hash = results[0].hash;
+
+    // check that passwords match
+    if (!corelib.checkPasswordHash(password,hash)){
+        // Password does not match
+        console.log('Error: Password does not match');
+        res.status(500).json({error: 207, message:'Password does not match'});
+        db.disconnect();
+        return;
+    }
+
+    // check if user is already logged in
+    var user_id = results[0].user_id;
+    var username = results[0].username;
+    var qry = "SELECT token_id FROM reclodb.tokens WHERE user_id = ? AND token_status = 'A'";
+    var params = [user_id];
+
+function checkLoginStatusCallback(err,results) {
+
+    if (err) {
+        console.log('checkLoginStatus ' + err);
+        res.status(500).json({error: 101, message:'There was an error connecting to the database'}); // MySQL error
+        db.disconnect();
+        return;
+    }
+
+    var already_logged_in = false;
+    if (results.length != 0) {
+        // user already logged in!
+        already_logged_in = true;
+        var old_token = results[0].token_id;
+        console.log('User aleady logged in. Generating new token.');
+    }
+
+    // login user, add token to token table
+    var qry = "INSERT INTO reclodb.tokens SET date_created = NOW(), ?";
+
+    var token_id = corelib.createToken();
+
+    var params = {
+        'token_id'      : token_id,
+        'user_id'       : user_id,
+        'token_status'  : 'A',
+    };
+
+function createTokenCallback() {
+
+    if (err) {
+        console.log('createToken' + err);
+        res.status(500).json({error: 101, message:'There was an error connecting to the database'}); // MySQL error
+        db.disconnect();
+        return;
+    }
+
+    console.log('Login successful');
+
+    if (already_logged_in) {
+
+        // invalidate old token in Token table
+        var qry = "UPDATE reclodb.tokens SET token_status = 'D', date_deactivated = NOW() WHERE token_id = ?";
+        var params = [old_token];
+
+    function invalidateTokenCallback(err,results) {
 
         if (err) {
-            console.log('There was an error connecting to the database: ' + err);
-            res.status(500).json({error: 101, message: 'There was an error connecting to the database'});
+            console.log('deactivateToken ' + err);
+            res.status(500).json({error: 211, message: 'Failed to deactivate session token'}); // MySQL error
             db.disconnect();
             return;
         }
 
-        // verify user in database
-        var qry = "SELECT * FROM reclodb.users WHERE email = ? AND user_status = 'A'";
-        var params = [email];
+        console.log('Old token deactivated');
+        res.status(200).json({user_id: user_id, username: username, token: token_id, message:'login successful'});
+        db.disconnect();
 
-        function verifyUserCallback(err,results) {
+    }; // queryCallback
+    db.query(qry,params,invalidateTokenCallback);
 
-            if (err) {
-                console.log('verifyUser ' + err);
-                res.status(500).json({error: 101, message: 'There was an error connecting to the database'}); // MySQL error
-                db.disconnect();
-                return;
-            }
+    }
+    else {
+        res.status(200).json({user_id: user_id, username: username, token: token_id, message:'login successful'});
+        db.disconnect();
+    }
 
-            if (results.length == 0) {
-                // user not found
-                console.log('Error: User not found');
-                res.status(500).json({error: 206, message:'User not found'});
-                db.disconnect();
-                return;
-            }
+}; // createTokenCallback
+db.query(qry,params,createTokenCallback);
 
-            var hash = results[0].hash;
+}; // checkLoginStatusCallback
+db.query(qry,params,checkLoginStatusCallback);
 
-            // check that passwords match
-            if (!corelib.checkPasswordHash(password,hash)){
-                // Password does not match
-                console.log('Error: Password does not match');
-                res.status(500).json({error: 207, message:'Password does not match'});
-                db.disconnect();
-                return;
-            }
+}; // verifyUserCallback
+db.query(qry,params,verifyUserCallback);
 
-            // check if user is already logged in
-            var user_id = results[0].user_id;
-            var qry = "SELECT token_id FROM reclodb.tokens WHERE user_id = ? AND token_status = 'A'";
-            var params = [user_id];
+}; // connectionCallback
 
-            function checkLoginStatusCallback(err,results) {
-
-                if (err) {
-                    console.log('checkLoginStatus ' + err);
-                    res.status(500).json({error: 101, message:'There was an error connecting to the database'}); // MySQL error
-                    db.disconnect();
-                    return;
-                }
-
-                var already_logged_in = false;
-                if (results.length != 0) {
-                    // user already logged in!
-                    already_logged_in = true;
-                    var old_token = results[0].token_id;
-                    console.log('User aleady logged in. Generating new token.');
-                }
-
-                // login user, add token to token table
-                var qry = "INSERT INTO reclodb.tokens SET date_created = NOW(), ?";
-
-                var token_id = corelib.createToken();
-
-                var params = {
-                    'token_id'      : token_id,
-                    'user_id'       : user_id,
-                    'token_status'  : 'A',
-                };
-
-                function createTokenCallback() {
-
-                    if (err) {
-                        console.log('createToken' + err);
-                        res.status(500).json({error: 101, message:'There was an error connecting to the database'}); // MySQL error
-                        db.disconnect();
-                        return;
-                    }
-
-                    console.log('Login successful');
-
-                    if (already_logged_in) {
-
-                        // invalidate old token in Token table
-                        var qry = "UPDATE reclodb.tokens SET token_status = 'D', date_deactivated = NOW() WHERE token_id = ?";
-                        var params = [old_token];
-
-                        function invalidateTokenCallback(err,results) {
-
-                            if (err) {
-                                console.log('deactivateToken ' + err);
-                                res.status(500).json({error: 211, message: 'Failed to deactivate session token'}); // MySQL error
-                                db.disconnect();
-                                return;
-                            }
-
-                            console.log('Old token deactivated');
-                            res.status(200).json({user_id: user_id, token: token_id, message:'login successful'});
-                            db.disconnect();
-
-                        }; // queryCallback
-                        db.query(qry,params,invalidateTokenCallback);
-                    }
-                    else {
-                        res.status(200).json({user_id: user_id, token: token_id, message:'login successful'});
-                        db.disconnect();
-                    }
-
-                }; // createTokenCallback
-                db.query(qry,params,createTokenCallback);
-
-            }; // checkLoginStatusCallback
-            db.query(qry,params,checkLoginStatusCallback);
-
-        }; // verifyUserCallback
-        db.query(qry,params,verifyUserCallback);
-
-    }; // connectionCallback
-
-    // Open MySQL database connection
-    var db = new DBConnection();
-    db.connect(connectionCallback.bind(db));
+// Open MySQL database connection
+var db = new DBConnection();
+db.connect(connectionCallback.bind(db));
 
 }); // router
 
