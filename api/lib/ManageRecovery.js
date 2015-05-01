2e50827ed693f54faa67d0b505c131f093232635
Carlton Duffett <cjduffett@comcast.net>
2015-05-01 15:44:10 -0400

    Updates
    Removed Merging and Merged states
    Completed/tested pending, downloading handlers

--- api/lib/ManageRecovery.js
+++ api/lib/ManageRecovery.js
@@ -12,9 +12,7 @@
  * -------------------------------------------------------------------------------------------------
  * PENDING      - ( 0%) waiting to start backup downloads
  * DOWNLOADING  - ( - ) downloading full and incremental backups to temporary storage for import
- * DOWNLOADED   - (15%) ready to merge downloaded backups into one VHD
- * MERGING      - ( - ) merging VHD incrementals into one full backup for import
- * MERGED       - (25%) ready to import backup into EC2
+ * DOWNLOADED   - (35%) ready to merge downloaded backups into one VHD
  * IMPORTING    - ( - ) importing backup into EC2, see progress using ec2-describe-conversion-tasks
  * IMPORTED     - (55%) sucessfully imported into EC2, ready to start conversion
  * CONVERTING   - ( - ) in-progress conversion from VHD to EC2 AMI instance (started automatically)
@@ -27,6 +25,7 @@
 
 var DBConnection = require('../lib/DBConnection.js');
 var corelib = require('../lib/core.js');
+var fs = require('fs');
 
 var AWS = require('aws-sdk');
 AWS.config.region = 'us-west-2';
@@ -35,7 +34,11 @@ var ts = corelib.createTimestamp();
 console.log('Starting recovery management at ' + ts);
 
 // current progress percentage weights
-var progress = {downloading: 5, downloaded: 15, merged: 25, imported: 55, converted: 90, finished: 100};
+var progress = {downloading: 10,
+                downloaded: 35,
+                imported: 55,
+                converted: 90,
+                finished: 100};
 
 /* -------------------------------------------------------------------------------------------------
  * MANAGE ALL ACTIVE RECOVERY TASKS
@@ -50,7 +53,7 @@ function connectionCallback(err) {
     }
     
     // get a list of current recovery tasks in progress
-    var qry = "SELECT recovery_id, user_id, recovery_state FROM reclodb.recovery WHERE " +
+    var qry = "SELECT recovery_id, user_id, backup_id, recovery_state FROM reclodb.recovery WHERE " +
               "recovery_state != 'finished' AND " +
               "recovery_state != 'failed' AND " + 
               "recovery_status = 'A'";
@@ -72,31 +75,24 @@ function connectionCallback(err) {
         // update the progress and advance each recovery task
         for (i = 0; i < results.length; i++) {
             
-            this_state  = results[i].recovery_state;
-            user_id     = results[i].user_id;
-            recovery_id = results[i].recovery_id;
+            var this_state  = results[i].recovery_state;
+            var user_id     = results[i].user_id;
+            var recovery_id = results[i].recovery_id;
+            var backup_id   = results[i].backup_id;
             
             switch (this_state) {
                 
                 case 'pending':
-                    handlePending(user_id,recovery_id);
+                    handlePending(user_id,recovery_id,backup_id);
                     break;               
                                 
                 case 'downloading':
-                    handleDownloading(user_id,recovery_id);
+                    handleDownloading(recovery_id);
                     break;               
                                 
                 case 'downloaded':
-                    handleDownloaded(user_id,recovery_id);
+                    handleDownloaded(recovery_id);
                     break;               
-                                
-                case 'merging':
-                    handleMerging(user_id,recovery_id);
-                    break;               
-                                
-                case 'merged':
-                    handleMerged(user_id,recovery_id);
-                    break;
                     
                 case 'importing':
                     handleImporting(user_id,recovery_id);
@@ -129,7 +125,7 @@ db.connect(connectionCallback.bind(db));
  * HANDLE PENDING RECOVERY TASKS
  * -------------------------------------------------------------------------------------------------
  */
-function handlePending(user_id,recovery_id) {
+function handlePending(user_id,recovery_id,backup_id) {
 
     console.log('Handling PENDING recovery task ' + recovery_id);
 
@@ -157,6 +153,7 @@ function handlePending(user_id,recovery_id) {
                     "SELECT file_name FROM reclodb.backups WHERE " +
                         "backup_status = 'A' AND " + 
                         "user_id = ? AND " + 
+                        "type = 'full' AND " + // just grab the full backup for now
                         "date_created BETWEEN @date_of_last_full AND @date_selected;";
                                
         var params = [backup_id,user_id];
@@ -171,7 +168,7 @@ function handlePending(user_id,recovery_id) {
                 return;
             }
             
-            backups = results.splice(0,2); // first 2 results are garbage from the "SET" statements
+            backups = results[2]; // first 2 results (0-1) are garbage from the "SET" statements
             
             var qry = "UPDATE reclodb.recovery SET no_downloads = ?, " +
                         "no_completed = 0, " + 
@@ -187,18 +184,111 @@ function handlePending(user_id,recovery_id) {
                     console.log('Recovery management failed for task ' + recovery_id + 
                                 ', there was an error connecting to the database');
                     
-                    db.disconnect();
+                    db.disconnect(); 
                     return;
                 }               
                 
-                // initiate parallel download of full and incremental backups
-                for (i = 0; i < backups.length; i++) {
-                    
-                    console.log('Downloading file ' + backups[i].file_name);
-                    
-                } // for 
+                // download full backup
+                console.log('Downloading file ' + backups[0].file_name);
                 
-                db.disconnect();   
+                var dir = '/backups-tmp/' + user_id + '/';
+
+                if (!fs.existsSync(dir)){
+                    fs.mkdirSync(dir);
+                }
+                
+                // backups download to separate volume mounted on our server 
+                var file_name   = backups[0].file_name;
+                var path        = '/backups-tmp/' + user_id + '/' + file_name;  
+                var stream      = fs.createWriteStream(path);     
+                var bucket      = 'reclo-client-backups';
+                var key         = user_id + '/' + file_name;
+                
+                var download = {
+                    path        : path,
+                    stream      : stream,
+                    file_name   : file_name,
+                    no_chunks   : 0
+                }
+                
+                var params = {
+                    Bucket  : bucket,
+                    Key     : key
+                };
+        
+                var s3 = new AWS.S3();
+
+                function getObjectErrorHandler(err,response) {
+                        
+                    console.log('prepareBackup Error: ' + err);
+                    console.log('download failed for file ' + download.file_name);
+                    download.stream.end();
+                    
+                    // update database to indicate failure
+                    var qry = "UPDATE reclodb.recovery SET recovery_state = 'failed' " +
+                              "WHERE recovery_id = ?"; 
+                                      
+                    var params = [recovery_id];
+                    
+                    function failRecoveryCallback(err,results) {
+                        
+                        db.disconnect();
+                        
+                        if (err) {
+                            console.log('Recovery management failed for task ' + recovery_id + 
+                                        ', there was an error connecting to the database');
+                            console.log('Management failed while setting recovery ' + recovery_id + ' to FAILED.');
+                            return;
+                        }                             
+                    } // failRecoveryCallback        
+                    db.query(qry,params,failRecoveryCallback); 
+                                    
+                } // getObjectErrorHandler
+
+                function getObjectChunkHandler(chunk) {
+                    
+                    download.stream.write(chunk);
+                    download.no_chunks++;
+                    process.stdout.write("Downloaded " + download.no_chunks + " chunks\r");
+                          
+                } // getObjectChunkHandler
+
+                function getObjectDoneHandler(response) {
+                    
+                    console.log('Prepare of ' + download.file_name + 
+                                ' completed with ' + download.no_chunks + 
+                                ' chunks transferred');  
+                                              
+                    download.stream.end();
+                    
+                    // update database, all downloads completed
+                    var qry = "UPDATE reclodb.recovery SET no_completed = 1, " +
+                                "state_progress = 100 " + // 100% complete
+                                "WHERE recovery_id = ?"; 
+                                                 
+                    var params = [recovery_id];
+                    
+                    function completeDownloadCallback(err,results) {
+                        
+                        db.disconnect();
+                        
+                        if (err) {
+                            console.log('Recovery management failed for task ' + recovery_id + 
+                                        ', there was an error connecting to the database');
+                            console.log('Management failed while setting recovery ' + recovery_id + ' to DOWNLOADED.');
+                            return;
+                        }                    
+                    } // completeDownloadCallback
+                    db.query(qry,params,completeDownloadCallback); 
+                                                                       
+                } // getObjectDoneHandler
+                
+                // start download from s3
+                var request = s3.getObject(params);
+                request.on('error', getObjectErrorHandler);
+                request.on('httpData', getObjectChunkHandler);
+                request.on('httpDone', getObjectDoneHandler);
+                request.send();
             }  
             db.query(qry,params,startDownloadCallback);
             
@@ -206,15 +296,14 @@ function handlePending(user_id,recovery_id) {
         db.query(qry,params,getBackupListCallback);
     }
     var db = new DBConnection();
-    db.connect(connectionCallback.bind(db));   
-    
+    db.connect(connectionCallback.bind(db));    
 }
 
 /* -------------------------------------------------------------------------------------------------
  * HANDLE DOWNLOADING RECOVERY TASKS
  * -------------------------------------------------------------------------------------------------
  */
-function handleDownloading(user_id,recovery_id) {
+function handleDownloading(recovery_id) {
 
     console.log('Handling DOWNLOADING recovery task ' + recovery_id);
     
@@ -229,7 +318,7 @@ function handleDownloading(user_id,recovery_id) {
         }
         
         // get a list of current download tasks
-        var qry = "SELECT no_downloads, no_completed FROM reclodb.recovery WHERE recovery_id = ?";
+        var qry = "SELECT no_downloads, state_progress, no_completed FROM reclodb.recovery WHERE recovery_id = ?";
         var params = [recovery_id];
 
         function getDownloadProgressCallback(err,results) {
@@ -242,8 +331,9 @@ function handleDownloading(user_id,recovery_id) {
                 return;
             }
             
-            no_downloads = results[0].no_downloads;
-            no_completed = results[0].no_completed;
+            no_downloads   = results[0].no_downloads;
+            no_completed   = results[0].no_completed;
+            state_progress = results[0].state_progress;
             
             if (no_downloads == no_completed) {
                 
@@ -255,40 +345,32 @@ function handleDownloading(user_id,recovery_id) {
                  
                  
                 var next_state      = 'downloaded';
-                var state_progress  = 0
+                var state_progress  = 0;
                 var total_progress  = progress.downloaded;
                            
-                var params = [next_state, state_progress, total_progress, recovery_id];
-                
-                var msg = "Downloading completed";
-                
-            } else {
-                
-                // downloading still in progress
-                var qry = "UPDATE reclodb.recovery SET state_progress = ? WHERE recovery_id = ?";
-                
-                var state_progress = (no_completed / no_downloaded) * 100;
-                
-                var params = [state_progress, recovery_id];
-                
-                var msg = "Downloading progress updated";
-            }
+                var params = [next_state, state_progress, total_progress, recovery_id];  
             
-            function updateDownloadingProgressCallback(err,results) {
-                
-                if (err) {
-                    console.log('Recovery management failed for task ' + recovery_id + 
-                                ', there was an error connecting to the database');
+                function completeDownloadCallback(err,results) {
                     
+                    if (err) {
+                        console.log('Recovery management failed for task ' + recovery_id + 
+                                    ', there was an error connecting to the database');
+                        
+                        db.disconnect();
+                        return;
+                    }
+                    
+                    console.log('Download complete for recovery ' + recovery_id);
                     db.disconnect();
-                    return;
-                }
+                                   
+                } // updateDownloadingProgress
+                db.query(qry,params,completeDownloadCallback);
+            }
+            else {
                 
-                console.log(msg);
+                console.log('Download still in-progress.');
                 db.disconnect();
-                               
-            } // updateDownloadingProgress
-            db.query(qry,params,updateDownloadingProgressCallback);
+            } // if
             
         } // getRecoveryTasksCallback
         db.query(qry,params,getDownloadProgressCallback);
@@ -301,7 +383,7 @@ function handleDownloading(user_id,recovery_id) {
  * HANDLE DOWNLOADED RECOVERY TASKS
  * -------------------------------------------------------------------------------------------------
  */
-function handleDownloaded(user_id,recovery_id) {
+function handleDownloaded(recovery_id) {
 
     console.log('Handling DOWNLOADED recovery task ' + recovery_id);
 
@@ -317,19 +399,19 @@ function handleDownloaded(user_id,recovery_id) {
         var qry = "UPDATE reclodb.recovery SET recovery_state = 'merging', state WHERE recovery_id = ?";
         var params = [recovery_id];
 
-        function startMergingCallback(err,results) {
+        function startImportCallback(err,results) {
         
             if (err) {
                 console.log('Recovery management failed for task ' + recovery_id + ', there was an error connecting to the database');
                 return;
             }
             
-            // begin merging task here
-            var merging = 'foo';
+            // begin import task here
+            var import_task = 'foo';
             
             
         } // getRecoveryTasksCallback
-        db.query(qry,params,startMergingCallback);
+        db.query(qry,params,startImportCallback);
     }
     var db = new DBConnection();
     db.connect(connectionCallback.bind(db));
@@ -339,7 +421,7 @@ function handleDownloaded(user_id,recovery_id) {
  * HANDLE MERGING RECOVERY TASKS
  * -------------------------------------------------------------------------------------------------
  */
-function handleMerging(user_id,recovery_id) {
+/*function handleMerging(user_id,recovery_id) {
 
     console.log('Handling MERGING recovery task ' + recovery_id);
 
@@ -361,7 +443,7 @@ function handleMerging(user_id,recovery_id) {
                 return;
             }
             
-            // do something polite here
+            // do something here
             
         } // getRecoveryTasksCallback
         db.query(qry,getRecoveryTasksCallback);
@@ -374,7 +456,7 @@ function handleMerging(user_id,recovery_id) {
  * HANDLE MERGED RECOVERY TASKS
  * -------------------------------------------------------------------------------------------------
  */
-function handleMerged(user_id,recovery_id) {
+/*function handleMerged(user_id,recovery_id) {
 
     console.log('Handling MERGED recovery task ' + recovery_id);
 
@@ -401,7 +483,7 @@ function handleMerged(user_id,recovery_id) {
                 return;
             }
             
-            // do something polite here
+            // do something here
             
         } // getRecoveryTasksCallback
         db.query(qry,getRecoveryTasksCallback);
